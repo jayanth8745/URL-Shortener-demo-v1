a63fe534e517efc64a8ac3863cae442d1b20eef7
jayanth8745 <jayanth8745@gmail.com>
2026-03-27 14:02:55 +0530

    Banckend update

--- Backend/server.js
+++ Backend/server.js
@@ -1,19 +1,23 @@
+require("dotenv").config({ path: "./Backend/.env" });
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const urlRoutes = require("./routes/urlRoutes");
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 
-// Connect to MongoDB
-mongoose.connect("mongodb+srv://admin:xCvcZjwUfMZPBgjZ@cluster0.vfgnckp.mongodb.net/?appName=Cluster0")
+console.log("ENV:", process.env.MONGO_URI);
+
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
-// Routes
 app.use("/api", urlRoutes);
 
-// Start server
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
