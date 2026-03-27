5ce2dda57be3f0d21326b45d4d759f2f4c4eeff8
jayanth8745 <jayanth8745@gmail.com>
2026-03-27 14:28:49 +0530

    final backend fix

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,4 @@
-require("dotenv").config({ path: "./Backend/.env" });
+require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
@@ -9,8 +9,6 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-
-console.log("ENV:", process.env.MONGO_URI);
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
